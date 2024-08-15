27860330b852d03fc6df005663cfdda9c4f66042
Navid_Rahmani <navidrahmani770@gmail.com>
2024-08-15 18:40:14 +0330

    customizing form validation

--- A/blog/forms.py
+++ A/blog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from blog.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -8,6 +9,17 @@ class RegistrationForm(FlaskForm):
     email = StringField('Email',validators=[DataRequired(), Email(message='Enter valid email like exampel@mail.com')])
     password = PasswordField('Password',validators=[DataRequired(), Length(min=4, max=30, message='Enter password contains 4 charachter at least')])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message='Password must match')])
+
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('this username already exists')
+        
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("This email already exists")
 
 
 class LoginForm(FlaskForm):

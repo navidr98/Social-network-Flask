52287fc91c9e8f9c803f0da646d09e7f290bb120
Navid_Rahmani <navidrahmani770@gmail.com>
2024-08-24 12:32:43 +0330

    login and register and form errors/validations completed

--- A/blog/forms.py
+++ A/blog/forms.py
@@ -1,20 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from blog.models import User
+from flask_wtf.recaptcha import RecaptchaField
+
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username',validators=[DataRequired(message='Enter username'), Length(min=3, max=30, message='Username must contain at least for 3 charachters')])
+    username = StringField('Username',validators=[DataRequired(), Length(min=3, max=30, message='Username must be between 3 and 30 characters long.')])
     email = StringField('Email',validators=[DataRequired(), Email(message='Enter valid email like exampel@mail.com')])
     password = PasswordField('Password',validators=[DataRequired(), Length(min=4, max=30, message='Enter password contains 4 charachter at least')])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message='Password must match')])
+    submit = SubmitField('Register')
 
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError('this username already exists')
+            raise ValidationError('This username already exists')
         
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
@@ -23,6 +26,6 @@ class RegistrationForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email',validators=[DataRequired(), Email()])
-    password = PasswordField('Password',validators=[DataRequired(), Length(min=4, max=30)])
-    remember = BooleanField('Remember me')
+    email = StringField('Email',validators=[DataRequired(), Email(message='Enter valid email like exampel@mail.com')])
+    password = PasswordField('Password',validators=[DataRequired(), Length(min=4, max=30, message='Enter password contains 4 charachter at least')])
+    submit = SubmitField('Login')
--- A/blog/routes.py
+++ A/blog/routes.py
@@ -1,7 +1,7 @@
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request
 from blog import app, db, bcrypt
 from blog.forms import RegistrationForm, LoginForm
-from blog.models import User
+from blog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
 
@@ -13,33 +13,44 @@ def home():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-
+    
     if current_user.is_authenticated:
         return redirect(url_for('home'))
+    
     form = RegistrationForm()
-
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash('form.password.data').decode('utf-8')
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
+        next_page = request.args.get('next')
         flash('You registered successfully', 'success')
-        return redirect(url_for('home'))
+        return redirect(next_page if next_page else url_for('home'))
     return render_template('register.html', form=form)
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
+
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember.data)
+            login_user(user)
+            next_page = request.args.get('next')
             flash('You logged in successfully', 'success')
-            return redirect(url_for('home'))
+            return redirect(next_page if next_page else url_for('home'))
         else:
             flash('Email or Password is wrong', 'danger')
     return render_template('/login.html', form=form)
 
 
+@app.route('/logout')
+@login_required
+def logout():
 
-
+    logout_user()
+    flash('You logged out successfully', 'success')
+    return redirect(url_for('home'))
